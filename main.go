e84a63cfd7f7e51a0ba771873ce11d3412d34e6a
Prabesh Thapa <sthapaprabesh2020@gmail.com>
2022-10-31 10:27:07 -0700

    Graceful error handling

--- main.go
+++ main.go
@@ -1,6 +1,9 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+)
 
 func thirdPartyFunction() {
 	fmt.Println("error occured here and we need tp panic")
@@ -20,7 +23,21 @@ func start() {
 	thirdPartyFunction()
 }
 
+func errorReturningFucntion(flag bool) error {
+	if !flag {
+		fmt.Println("This function returns an error")
+		return fmt.Errorf("flag is set to false")
+	}
+	return nil
+}
+
 func main() {
+	flag := false
 	start()
 	fmt.Println("Application recoverd and regained contropl and application can still continue")
+
+	err := errorReturningFucntion(flag)
+	if err != nil {
+		log.Fatal("Error occured in function errorReturningFunction", err)
+	}
 }
